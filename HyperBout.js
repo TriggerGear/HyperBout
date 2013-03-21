6adbf0da7cd264b77b8109f5adb159ecd300f24d
Philip Vaca <vaca.philip@gmail.com>
2013-03-20 18:51:47 -0700

    Simple engine for drawing text and animating it

--- HyperBout.js
+++ HyperBout.js
@@ -1,17 +1,18 @@
 
 var hyperBout = function()
 {
-	
-    return new CanvasWrapper('backgroundCanvas', 1122, 548);
+    this.width = 1122;
+    this.height = 548;
+    return new CanvasWrapper('backgroundCanvas', width, height);
 };
 
 var Engine = function()
-{	
-	//Test the update and draw methods
-	var testX = 0;
-	var testY = 0;
+{   
+    //Test the update and draw methods
+    this.textX = 0;
+    this.textY = 0;
 
-	//Create a new hyperBout object.
+    //Create a new hyperBout object.
     this.hyperBout = hyperBout();
 
     //Create an image and set the source to the background, add it to HyperBout canvas context
@@ -20,30 +21,38 @@ var Engine = function()
     this.hyperBout.ctx.drawImage(backgroundImg, 0, 0);
 
     //Create the player
-    var player = new Player();
+    this.player = new Player();
     
     //Start the engine
-    this.start(this);
+    this.start();
 };
-//Set the frames per second to 30
+//Set the frames per second to 
 var FPS = 30;
-Engine.prototype.start = function(engine)
+Engine.prototype.start = function()
 {
-	setInterval(function()
-	{
-		update(engine);
-		draw(engine);
-	}, 1000/30);
+    var self = this;
+    setInterval(function()
+    {
+        self.update();
+        self.draw();
+    }, 1000/FPS);
 };
 //Draw text to test updating
-function draw(args){
-	args.hyperBout.canvas.fillStyle = "#000";
-	//args.hyperBout.canvas.fillText("TESTING", 0, 0);
+Engine.prototype.draw = function()
+{
+	this.hyperBout.ctx.clearRect(0, 0, this.hyperBout.width, this.hyperBout.height);
+	 //Create an image and set the source to the background, add it to HyperBout canvas context
+    var backgroundImg = new Image();
+    backgroundImg.src = 'images/Background.png';
+    this.hyperBout.ctx.drawImage(backgroundImg, 0, 0);
+    this.hyperBout.ctx.fillStyle = "#ffffff";
+    this.hyperBout.ctx.fillText("TESTING", this.textX, this.textY);
 }
 //Move the text diagonal
-function update(args){
-	args.textX += 1;
-	args.textY += 1;
+Engine.prototype.update = function()
+{
+    this.textX += 1;
+    this.textY += 1;
 }
 //Canvas wrapper
 function CanvasWrapper(domId, width, height) {
@@ -54,23 +63,22 @@ function CanvasWrapper(domId, width, height) {
 }
 //Player class
 var Player = function(){
-	var healthPoints = 5;
-	var playerNumber = 1;
-	//Movement and location variables
-	var xpos = 100;
-	var ypos = 100;
-	var xspeed = 1;
-	var yspeed = 0;
+    this.healthPoints = 5;
+    this.playerNumber = 1;
+    //Movement and location variables
+    this.xpos = 100;
+    this.ypos = 100;
+    this.xspeed = 1;
+    this.yspeed = 0;
+    //Maximum Boundary Variables
+    var minx = 0;
+    var miny = 0;
+    var maxx = 1122;
+    var maxy = 548;
 
-	//Maximum Boundary Variables
-	var minx = 0;
-	var miny = 0;
-	var maxx = 1122;
-	var maxy = 548;
-
-	//Player image
-	var playerImage = new Image();
-	playerImage.src = 'images/playerStationary.png';
+    //Player image
+    var playerImage = new Image();
+    playerImage.src = 'images/playerStationary.png';
 
 };
 //Movement function for players
@@ -80,6 +88,5 @@ Player.prototype.move = function(args)
 }
 //Sets the location for each player based on the player number.
 Player.prototype.setLocation = function(playerList){
-	
+    
 }
-
